e59df792ef917d9e83d1cf09c04ae843fbc95894
mazag12 <maxter_anime12@outlook.com>
2024-03-14 20:16:33 -0500

    ADD SHARED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
